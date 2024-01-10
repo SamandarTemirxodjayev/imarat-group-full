9498198d90c600f738d4648a8eb33dc5cb77f93c
Akbar Odilov <140045856+odilov-a@users.noreply.github.com>
2024-01-10 12:29:17 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
-
 const router = express.Router();
 
-// Public routes
-
-
-// Protected routes (requires authentication)
-router.use(authMiddleware.authenticateToken);
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
+router.use(authMiddleware.authenticateToken);
 router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
