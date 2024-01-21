8e51ee4223dc98f28255bfdecaa2c97bfd6a34ba
Akbar Odilov <140045856+odilov-a@users.noreply.github.com>
2024-01-21 19:09:47 +0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,9 @@ const adminController = require("../controllers/adminController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
-
-// Protected routes (requires authentication)
-// router.use(authMiddleware.authenticateToken);
 router.get("/", adminController.getAllAdmins);
 router.get("/:id", adminController.getAdminById);
+router.use(authMiddleware.authenticateToken);
 router.post("/", adminController.createAdmin);
 router.put("/:id", adminController.updateAdmin);
 router.delete("/:id", adminController.deleteAdmin);
